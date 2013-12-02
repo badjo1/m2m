664a1def2d885e61ccf65ab3668a781dc31b295f
Bart Bloemers <bart@badjo.nl>
2013-12-02 14:34:26 +0100

    Initialize repository

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+M2m::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+M2m::Application.config.secret_key_base = '50bcec63cc2ef9377242274a43cec6e4698f9823bcf0752230d93434443701d6f3af80c21437c036f13b35c9dc46e3888786f668260855ce37e6e455672387a3'
