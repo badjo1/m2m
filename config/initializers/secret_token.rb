5bdd75742d8e502099fcc84d525b0c2f77c49ce8
Bart Bloemers <bart@badjo.nl>
2013-12-03 13:44:29 +0100

    move secret token into file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,21 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-M2m::Application.config.secret_key_base = '50bcec63cc2ef9377242274a43cec6e4698f9823bcf0752230d93434443701d6f3af80c21437c036f13b35c9dc46e3888786f668260855ce37e6e455672387a3'
+
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+M2m::Application.config.secret_key_base = secure_token
